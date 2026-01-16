7e03e38416a9edf89bf7df481c7ad804aeb07019
rodrigueclemencet-lang <rodrigueclemencet@gmail.com>
2026-01-16 09:32:51 +0000

    Programme principal

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const float coutHoraireMeca = 4.0;
+  const float coutHoraireElec = 5.0;
+  int velosDispos = 20;
+  int capacite= 20;
+  int choix;
+  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+  cout << "-----------------------------------------" << endl;
+  //Partie à compléter 3.1
+
+
+  cout << "-----------------------------------------" << endl<< endl;
+  cout << "1. Emprunter un vélô" << endl;
+  cout << "2. Restituer un vélô" << endl;
+  cout << "3. Estimer le coût de la location" << endl;	
+  cout << "4. Quitter l'application" << endl << endl;
+  return 0;
+}
